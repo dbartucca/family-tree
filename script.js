bc3a2d0a6ba42e9173437abcd60539c09e875dd3
dbartucca <dbartucca3@gmail.com>
2025-04-21 10:16:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 async function fetchData() {
   const response = await fetch('data.json');
-  const data = await response.json();
-  return data;
+  return await response.json();
 }
 
-function createPersonCard(person) {
+function createPersonElement(person, id) {
   const div = document.createElement('div');
   div.className = 'person';
+  div.id = `person-${id}`;
   div.innerHTML = `
+    <div class="photo"></div>
     <strong>${person.name.first} ${person.name.middle || ''} ${person.name.last}</strong><br>
     <small>${person.birth.year || ''}</small><br>
     <em>${person.bio.desc}</em>
@@ -15,47 +16,83 @@ function createPersonCard(person) {
   return div;
 }
 
-function buildTree(data, rootId, container) {
-  const person = data[rootId];
-  if (!person) return;
-
-  const personCard = createPersonCard(person);
-  container.appendChild(personCard);
-
-  const childrenContainer = document.createElement('div');
-  childrenContainer.className = 'children';
-
-  if (person.relations.children && person.relations.children.length > 0) {
-    const connector = document.createElement('div');
-    connector.className = 'connector';
-    container.appendChild(connector);
-
-    container.appendChild(childrenContainer);
-
+function getGenerations(data) {
+  const childToParent = {};
+  for (const [id, person] of Object.entries(data)) {
     person.relations.children.forEach(childId => {
-      const childWrapper = document.createElement('div');
-      buildTree(data, childId, childWrapper);
-      childrenContainer.appendChild(childWrapper);
+      if (!childToParent[childId]) childToParent[childId] = [];
+      childToParent[childId].push(id);
     });
   }
+
+  const roots = Object.keys(data).filter(id => !(id in childToParent));
+  const generations = [];
+
+  function buildGen(ids, level) {
+    if (!ids.length) return;
+    generations[level] = generations[level] || [];
+    generations[level].push(...ids);
+
+    const nextGen = [];
+    ids.forEach(id => {
+      const children = data[id].relations.children || [];
+      nextGen.push(...children.map(String));
+    });
+
+    buildGen([...new Set(nextGen)], level + 1);
+  }
+
+  buildGen(roots, 0);
+  return generations;
+}
+
+function drawLines(svg, fromEl, toEl) {
+  const fromBox = fromEl.getBoundingClientRect();
+  const toBox = toEl.getBoundingClientRect();
+  const svgBox = svg.getBoundingClientRect();
+
+  const x1 = fromBox.left + fromBox.width / 2 - svgBox.left;
+  const y1 = fromBox.bottom - svgBox.top;
+  const x2 = toBox.left + toBox.width / 2 - svgBox.left;
+  const y2 = toBox.top - svgBox.top;
+
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("x1", x1);
+  line.setAttribute("y1", y1);
+  line.setAttribute("x2", x2);
+  line.setAttribute("y2", y2);
+  line.setAttribute("stroke", "#333");
+  line.setAttribute("stroke-width", "2");
+  svg.appendChild(line);
 }
 
 window.onload = async () => {
   const data = await fetchData();
-  const treeContainer = document.getElementById('tree');
+  const container = document.getElementById('tree');
+  const svg = container.querySelector('svg');
 
-  // Find root people (those who are not listed as a child)
-  const allChildren = new Set();
-  for (const id in data) {
-    const person = data[id];
-    (person.relations.children || []).forEach(cid => allChildren.add(cid));
-  }
+  const generations = getGenerations(data);
+  const idToEl = {};
 
-  const rootPeople = Object.keys(data).filter(id => !allChildren.has(parseInt(id)));
+  generations.forEach((gen, index) => {
+    const levelDiv = document.createElement('div');
+    levelDiv.className = 'level';
+    gen.forEach(id => {
+      const el = createPersonElement(data[id], id);
+      idToEl[id] = el;
+      levelDiv.appendChild(el);
+    });
+    container.appendChild(levelDiv);
+  });
 
-  rootPeople.forEach(rootId => {
-    const rootWrapper = document.createElement('div');
-    buildTree(data, rootId, rootWrapper);
-    treeContainer.appendChild(rootWrapper);
+  // Wait for layout
+  requestAnimationFrame(() => {
+    for (const [id, person] of Object.entries(data)) {
+      (person.relations.children || []).forEach(childId => {
+        if (idToEl[id] && idToEl[childId]) {
+          drawLines(svg, idToEl[id], idToEl[childId]);
+        }
+      });
+  });
   });
 };
